f4b6ce19c4854e0933478ff34abfdc75e8aec5ce
Sridharan312 <sridhar31293@gmail.com>
2025-04-17 11:03:01 +0530

    New lines get added

--- src/org/data/Sample.java
+++ src/org/data/Sample.java
@@ -2,6 +2,12 @@ package org.data;
 
 public class Sample {
 
+	public void addition(int a, int b) {
+
+		int c = a + b;
+		System.out.println("C = " + c);
+	}
+
 	public static void main(String[] args) {
 
 		int a = 100;
